1c0c878c9629453debb2b90de7eab40534062571
Christopher Herzog <cjh30@pct.edu>
2021-04-12 00:14:13 -0400

    Setup files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Buck-Off',
+    version='0.1.0',    
+    description='Prevent tailgators from tailing your butt in traffic.',
+    url='https://github.com/schnipdip/Buck-Off',
+    author='Chris Herzog',
+    author_email='cjh30@pct.edu',
+    license='MIT',
+    packages=['Buck-Off'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.6',
+    ],
+)
